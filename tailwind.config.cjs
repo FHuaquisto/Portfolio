0d572613e8e7ba98f8288b94330f233028cd4380
FHuaquisto <fhuaquisto.21@gmail.com>
2022-03-09 11:56:55 -0500

    :art: add project's colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    colors: {
+      secondary: {
+        100: '#585675',
+        200: '#6E6C92',
+        700: '#2E2B46',
+        800: '#222039',
+        900: '#1F1D36',
+      },
+      primary: '#EF6D6D',
+    }
   },
   plugins: [],
 }
